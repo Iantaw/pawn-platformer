1f5a140ae3d0bdeb4ed2b0a5020e72a83170d716
Iantaw <ian@tawileh.net>
2026-09-21 23:00:24 -0400

    finish game

--- player.gd
+++ player.gd
@@ -1,22 +1,29 @@
 extends CharacterBody2D
+signal hit
 
+@onready var hud = $"../HUD"
+@onready var hud_headline = $"../HUD/Headline"
+@onready var start_position = $"../StartPosition"
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-var spawn_position: Vector2
+const JUMP_VELOCITY = -300.0
+
+var is_dead: bool = false
 
 func _ready() -> void:
-	spawn_position = global_position
-		
-func die() -> void:
-	global_position = spawn_position
-	velocity = Vector2.ZERO
-	
-	
-	
-	
+	if not hud.start_game.is_connected(_on_start_game):
+		hud.start_game.connect(_on_start_game)
 
+func new_game() -> void:
+	is_dead = false
+	velocity = Vector2.ZERO
+	global_position = start_position.global_position
+	show()
+	hud.hide()
+	
 func _physics_process(delta: float) -> void:
+	if is_dead:
+		return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -25,8 +32,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -34,17 +39,36 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _on_killzone_body_shape_entered(
+	_body_rid: RID,
+	body: Node2D,
+	_body_shape_index: int,
+	_local_shape_index: int
+) -> void:
+	if body != self:
+		return
 	
+	_die()
+
+func _die() -> void:
+	hide()
+	is_dead = true
+	velocity = Vector2.ZERO
+	hud_headline.text = "You Died!"
+	hud.show()
+	hit.emit()
+
+func _on_win_body_entered(body: Node2D) -> void:
+	if body != self:
+		return
 	
-	for i in get_slide_collision_count():
-		var collision := get_slide_collision(i)
-		var collider := collision.get_collider()
-		print("collided with: ", collider)
-		if collider is TileMapLayer:
-			var coords: Vector2i = collider.get_coords_for_body_rid(collision.get_collider_rid())
-			print("Tile coords: ", coords)
-			var tile_data: TileData = collider.get_cell_tile_data(coords)
-			print("Tile data: ", tile_data)
-			if tile_data and tile_data.get_custom_data("hazard"):
-				print("HAZARD HIT")
-				die()
+	hide()
+	is_dead = true
+	velocity = Vector2.ZERO
+	hud_headline.text = "You Win!"
+	hud.show()
+	
+func _on_start_game() -> void:
+	new_game()
+	
